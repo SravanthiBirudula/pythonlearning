df63818076b45d2b08256b62d92f1a9af9c32af9
sravanthi-bi <sravanthi.birudula@adports.ae>
2026-07-29 14:56:56 +0400

    Add sixwhile.py to demonstrate counter implementation with while loops and user exit prompt

---
+++ loops/sixwhile.py
@@ -0,0 +1,14 @@
+#build a counter using while loop
+i=0
+while i<4:
+    print("Round :",i)
+    i+=1
+
+answer = ""
+while answer.lower() != "yes":
+    answer = input("Do you want to exit? (yes/no): ")
+print("thank you")
+
+
+#while true
+
